28f289227a04012c20dfb5cc12dcc70bd57cf557
Abhiuday Gupta <77210185+aeswibon@users.noreply.github.com>
2026-05-21 01:07:33 +0530

    llm-bridge 1.1.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/llm-bridge.rb
+++ Formula/llm-bridge.rb
@@ -1,9 +1,9 @@
 class LlmBridge < Formula
   desc "Use any AI IDE's model catalog from any OpenAI-compatible client"
   homepage "https://github.com/aeswibon/llm-bridge"
-  url "https://github.com/aeswibon/llm-bridge/releases/download/v1.1.0/llm-bridge-macos-arm64"
-  sha256 "f389006d7262e9b8104b0519306462a1748075802c0738ad2d6d8ca67385055b"
-  version "1.1.0"
+  url "https://github.com/aeswibon/llm-bridge/releases/download/v1.1.1/llm-bridge-macos-arm64"
+  sha256 "eddbc26719ad6a7dba683cc9141c739d29dd9896fc8a13c9541b90af8bff93b2"
+  version "1.1.1"
   license "MIT"
 
   def install
